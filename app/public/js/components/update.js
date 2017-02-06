c2d825ad6235157ef21b7d4e4834c1bafe9ddc8b
Vadim Malushko <man.malushko@gmail.com>
2017-02-06 11:42:39 +0200

    Update update.js

--- app/public/js/components/update.js
+++ app/public/js/components/update.js
@@ -5,25 +5,25 @@ module.exports = function () {
     function Menu(elem) {
         elem.onclick = function (e) {
             let self = e.target;
-            // if (self.className == 'user-delete') {
-            //     let userId = self.parentElement.parentElement.children[0].innerHTML;
-            //     userId = (Number(userId) - 1).toString();
-            //     alert(userId);
-            //     self.parentElement.parentElement.children[1].innerHTML = "DELETE";
-            //     for(let i = 0; i < 2; i++){
-            //         self.parentElement.parentElement.lastElementChild.remove();
-            //     }
-            //     let xhr = new XMLHttpRequest();
-            //     xhr.open('DELETE', `/users/${userId}`, true);
-            //     xhr.send();
-            //     xhr.onreadystatechange = function () {
-            //         if (this.readyState != 4) return;
-            //         if (this.status > 250) {
-            //             alert(this.status ? this.statusText : 'The request fails!!!');
-            //         }
-            //         else alert(this.statusText);
-            //     }
-            // }
+            if (self.className == 'user-delete') {
+               let userId = self.parentElement.parentElement.children[0].innerHTML;
+               userId = (Number(userId) - 1).toString();
+               alert(userId);
+               self.parentElement.parentElement.children[1].innerHTML = "DELETE";
+               for(let i = 0; i < 2; i++){
+                 self.parentElement.parentElement.lastElementChild.remove();
+               }
+            let xhr = new XMLHttpRequest();
+            xhr.open('DELETE', `/users/${userId}`, true);
+            xhr.send();
+            xhr.onreadystatechange = function () {
+            if (this.readyState != 4) return;
+            if (this.status > 250) {
+                alert(this.status ? this.statusText : 'The request fails!!!');
+            }
+            else alert(this.statusText);
+        }
+    }
             if(self.className == 'user-update') {
                 console.log("update!!!");
                 self.parentElement.parentElement.lastElementChild.previousElementSibling.style = "display:none";
